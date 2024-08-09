3c7efca27938edeccdaf3af87c81e504d24e17d9
Nsikak Ikot <sageikot@gmail.com>
2024-08-09 13:22:41 +0100

    feat: Messaging setup for twilio pro

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -7,10 +7,11 @@ import {
   DATABASE_ID,
   databases,
   ENDPOINT,
+  messaging,
   PATIENT_COLLECTION_ID,
   PROJECT_ID,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.type";
 import { revalidatePath } from "next/cache";
 
@@ -104,11 +105,33 @@ export const updateAppointment = async ({
         throw new Error("Appointment not found")
     } 
 
-    // TODO SMS notification
+    const smsMessage = `
+    Hi, it's Co-Med.
+    ${type === 'schedule'
+      ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}.`
+      : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason} Please reschedule`
+    }.`
+
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
   } catch (error) {
     console.log(error);
   }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+try {
+  const message = await messaging.createSms(
+    ID.unique(),
+    content,
+    [],
+    [userId]
+  )  
+
+  return parseStringify(message);
+} catch (error) {
+  console.log(error)
+}
 };
